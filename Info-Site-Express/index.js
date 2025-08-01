21466d9890bd70f49da8b66e6309584594097cc0
dcrnek1 <dario.crnek@gmail.com>
2025-08-01 09:39:10 +0200

    Revisiting info-site but using express

---
+++ Info-Site-Express/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('{*splat}', (req, res) => {
+    fs.readFile(path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url + '.html'), (err, content) => {
+        if (!err) {
+            res.send(content.toString())
+        } else {
+            fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                if (!err) {
+                    res.send(content.toString())
+                } else {
+                    res.status(500).send('Internal server error, file not found.');
+                }
+            })
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+})
