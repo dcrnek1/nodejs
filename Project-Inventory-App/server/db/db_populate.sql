4ba0f618160cbcdd73053f5149d3f9cd6d57d294
dcrnek1 <dario.crnek@gmail.com>
2025-10-30 18:02:08 +0100

    populate db queries

---
+++ Project-Inventory-App/server/db/db_populate.sql
@@ -0,0 +1,189 @@
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Horror', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Action', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Science fiction', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Thriller', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Drama', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Fantasy', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Animation', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Drama', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Documentary', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Adventure', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Romantic', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Comedy', NULL, '2025-10-30 13:58:37.244');
+INSERT INTO public.category
+("name", image_path, tstamp)
+VALUES('Mystery', NULL, '2025-10-30 13:58:37.244');
+
+
+
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Shawshank Redemption', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Godfather', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Dark Knight', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Godfather Part II', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('12 Angry Men', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Lord of the Rings: The Return of the King', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Schindler''s List', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Lord of the Rings: The Fellowship of the Ring', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Pulp Fiction', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Forrest Gump', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Fight Club', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Inception', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Matrix', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Interstellar', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Life Is Beautiful', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Pianist', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('Gladiator', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+INSERT INTO public.product
+("name", image_path, description, stock, tstamp)
+VALUES('The Intouchables', NULL, NULL, 0, '2025-10-30 14:01:01.645');
+
+
+
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(2, 17, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(4, 17, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 16, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(11, 15, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 15, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(3, 14, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(2, 13, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(13, 13, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(13, 12, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 12, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(4, 11, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(2, 11, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 10, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 9, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(2, 9, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(4, 9, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 8, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(6, 8, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(13, 7, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 7, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 6, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(6, 6, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(2, 5, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 4, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(13, 3, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(6, 3, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 2, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(1, 1, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(5, 1, '2025-10-30 14:04:10.886');
+INSERT INTO public.category_product
+(category_id, product_id, tstamp)
+VALUES(13, 1, '2025-10-30 14:04:10.886');
