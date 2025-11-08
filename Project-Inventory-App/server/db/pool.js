7b1865a8b2f1023e1f77d9a003690da2b087ccb2
dcrnek1 <dario.crnek@gmail.com>
2025-11-08 20:03:25 +0100

    Env variable db connection

--- Project-Inventory-App/server/db/pool.js
+++ Project-Inventory-App/server/db/pool.js
@@ -1,9 +1,11 @@
 const {Pool} = require("pg");
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "InventoryApp",
-  password: "asdasd",
-  port: 5432
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
