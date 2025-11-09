2d5cbc97e87dc8356b9fd73bb090493cb4fc586c
dcrnek1 <dario.crnek@gmail.com>
2025-11-09 10:34:05 +0100

    trigger autodeploy test

--- Project-Inventory-App/server/index.js
+++ Project-Inventory-App/server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;  
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
